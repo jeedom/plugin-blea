db071c837f527579e01e20df28785cc74e038b7a
Cédric Dupont <lunarok@gmail.com>
2016-12-04 19:51:48 +0100

    Update xiaomiscale.py

--- resources/blead/devices/xiaomiscale.py
+++ resources/blead/devices/xiaomiscale.py
@@ -15,12 +15,12 @@ class XiaomiScale():
 	def parse(self,data):
 		action={}
 		action['present'] = 1
-    measunit = data[22:24]
-    measured = int((data[26:28] + data[24:26]), 16) * 0.01
-    unit = ''
-    if measunit.startswith(('03', 'b3')): unit = 'lbs'
-    if measunit.startswith(('12', 'b2')): unit = 'jin'
-    if measunit.startswith(('22', 'a2')): unit = 'Kg' ; measured = measured / 2
+	measunit = data[22:24]
+	measured = int((data[26:28] + data[24:26]), 16) * 0.01
+	unit = ''
+	if measunit.startswith(('03', 'b3')): unit = 'lbs'
+	if measunit.startswith(('12', 'b2')): unit = 'jin'
+	if measunit.startswith(('22', 'a2')): unit = 'Kg' ; measured = measured / 2
 		action['unit'] = unit
 		action['value'] = measured
 		return action
