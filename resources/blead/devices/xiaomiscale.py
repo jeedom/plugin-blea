24522f86228251dd289b890c53f2437696a091d0
Cédric Dupont <lunarok@gmail.com>
2016-12-04 11:36:13 +0100

    Create xiaomiscale.py

---
+++ resources/blead/devices/xiaomiscale.py
@@ -0,0 +1,28 @@
+from bluepy.btle import Scanner, DefaultDelegate, Peripheral
+import time
+import logging
+import globals
+import struct
+
+class XiaomiScale():
+	def __init__(self):
+		self.name = 'xiaomiscale'
+
+	def isvalid(self,name,manuf=''):
+		if name.lower() == self.name:
+			return True
+			
+	def parse(self,data):
+		action={}
+		action['present'] = 1
+    measunit = data[22:24]
+    measured = int((data[26:28] + data[24:26]), 16) * 0.01
+    unit = ''
+    if measunit.startswith(('03', 'b3')): unit = 'lbs'
+    if measunit.startswith(('12', 'b2')): unit = 'jin'
+    if measunit.startswith(('22', 'a2')): unit = 'Kg' ; measured = measured / 2
+		action['unit'] = unit
+		action['value'] = measured
+		return action
+
+globals.COMPATIBILITY.append(XiaomiScale)
