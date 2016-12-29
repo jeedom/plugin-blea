31406c14a7aa3dfd5e7159521620b7eecfa464b7
Ludovic <ludovic@jeedom.com>
2016-12-29 21:14:34 +0100

    improvements for comaptibility list

--- resources/blead/devices/nut.py
+++ resources/blead/devices/nut.py
@@ -2,6 +2,7 @@ from bluepy.btle import Scanner, DefaultDelegate
 import time
 import logging
 import globals
+from multiconnect import Connector
 
 class Nut():
 	def __init__(self):
@@ -15,5 +16,19 @@ class Nut():
 		action={}
 		action['present'] = 1
 		return action
+	
+	def action(self,message):
+		mac = message['device']['id']
+		handle = message['command']['handle']
+		value = message['command']['value']
+		conn = Connector(mac)
+		conn.connect(type='random')
+		if not conn.isconnected:
+			conn.connect(type='random')
+			if not conn.isconnected:
+				return
+		conn.writeCharacteristic(handle,value)
+		conn.disconnect()
+		return
 
 globals.COMPATIBILITY.append(Nut)
