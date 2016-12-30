8eae07ba604455e10811c34bc8a6bc7bcaa2a234
Ludovic <ludovic@jeedom.com>
2016-12-30 18:31:58 +0100

    added some dependancy

--- resources/install.sh
+++ resources/install.sh
@@ -3,7 +3,7 @@ echo 0 > /tmp/dependancy_blea_in_progress
 echo "Launch install of blea dependancy"
 sudo apt-get update
 echo 50 > /tmp/dependancy_blea_in_progress
-sudo apt-get install -y python-pip 
+sudo apt-get install -y python-pip python-dev build-essential
 echo 66 > /tmp/dependancy_blea_in_progress
 sudo apt-get install -y libglib2.0-dev git
 echo 75 > /tmp/dependancy_blea_in_progress
