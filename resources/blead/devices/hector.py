0106272d6ba4671a98fa282503275363124ba19f
Alexis Savin <alexissavin@users.noreply.github.com>
2019-02-19 21:27:11 +0100

    Update hector.py
    Fixing wrong pressure report (seems it's a simple string, at least on my version of this device)

--- resources/blead/devices/hector.py
+++ resources/blead/devices/hector.py
@@ -35,11 +35,10 @@ class Hector():
 			temperature = int(str(hex(temperature[0])[2:].zfill(2) + hex(temperature[1])[2:].zfill(2)),16)
 			humidity = int(str(hex(humidity[0])[2:].zfill(2) + hex(humidity[1])[2:].zfill(2)),16)
 			logging.debug(str(pression))
-			pression = int(str(hex(pression[0])[2:].zfill(2) + hex(pression[1])[2:].zfill(2)+ hex(pression[2])[2:].zfill(2)+ hex(pression[3])[2:].zfill(2)),16)
-			battery = int(str(hex(battery[0])[2:].zfill(2) + hex(battery[1])[2:].zfill(2)),16)
+			pression = str(hex(pression[0])[2:].zfill(2),2) . str(hex(pression[1])[2:].zfill(2),2) . str(hex(pression[2])[2:].zfill(2),2) . str(hex(pression[3])[2:].zfill(2)),2)			battery = int(str(hex(battery[0])[2:].zfill(2) + hex(battery[1])[2:].zfill(2)),16)
 			result['temperature'] = float(temperature)/10
 			result['humidity'] = float(humidity)/10
-			result['pressure'] = float(pression)/100
+			result['pressure'] = float(pression)
 			result['batteryvoltage'] = round(float(battery)*0.00051029,2)
 			if round(float(battery)*0.00051029,2) > 3:
 				battery = 100
