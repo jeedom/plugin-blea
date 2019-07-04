7781ee0461a21fb1418796156a34d778797384c7
KipK <hotlobster75@gmail.com>
2019-07-04 10:33:45 +0200

    indent

--- resources/blead/blead.py
+++ resources/blead/blead.py
@@ -35,9 +35,9 @@ except ImportError:
 	sys.exit(1)
 
 try:
-    import queue
+	import queue
 except ImportError:
-    import Queue as queue
+	import Queue as queue
 
 class ScanDelegate(DefaultDelegate):
 	import globals
@@ -159,10 +159,10 @@ def listen():
 					globals.IGNORE[:] = []
 					globals.LAST_CLEAR = int(time.time())
 				if globals.LEARN_MODE:
-                    globals.SCANNER.start((passive=False))
+					globals.SCANNER.start((passive=False))
 					globals.SCANNER.process(3)
 				else:
-                    globals.SCANNER.start((passive=True))
+					globals.SCANNER.start((passive=True))
 					globals.SCANNER.process(0.3)
 				globals.SCANNER.stop()
 				if globals.SCAN_ERRORS > 0:
