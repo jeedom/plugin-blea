ce237ec95cfcfdfb7bb6f473533b3be3f2f1c229
sarakha63 <sarakha_ludovic@yahoo.fr>
2019-07-24 15:19:09 +0200

    pass to 2.5 daemon version

--- core/class/blea.class.php
+++ core/class/blea.class.php
@@ -20,7 +20,7 @@
 class blea extends eqLogic {
 	/*     * ***********************Methode static*************************** */
 	public static $_widgetPossibility = array('custom' => true);
-	public static $_version = '2.4';
+	public static $_version = '2.5';
 	public static $_bluepy_version = '1.1.4';
 	public static function createFromDef($_def) {
 		event::add('jeedom::alert', array(
