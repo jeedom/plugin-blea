ebe33a5b9425877c46379791a6d8c1412c5f4cfa
Erwan N <enozay.dev@gmail.com>
2020-10-12 22:37:01 +0200

    modify lywsd03 (Xiaomi Mijia lywsd03mmc Temperature Humidity Sensor) to fix parsed data acquired by bluetooth

--- resources/blead/devices/lywsd03.py
+++ resources/blead/devices/lywsd03.py
@@ -22,6 +22,35 @@ class Lywsd03():
 	def parse(self,data,mac,name,manuf):
 		action={}
 		action['present'] = 1
+                if data.lower().startswith("95fe"):
+			##todo parse data
+			logging.debug('Lywsd03 PARSE data: ' + data )
+			val_type = data[28:30].lower()
+			val_len =  data[32:34]
+			val_data = data[34:42]
+			if val_type in ['04']:	 # type: temperature
+				t_data = val_data[2:4] + val_data[0:2]
+				temp = int(t_data,16)/10.0
+				logging.debug('Lywsd03------ Advertising Data=> Temp' + str(temp))
+				action['temperature'] = temp
+			elif val_type in ['06']: # type: moisture
+				h_data = val_data[2:4] + val_data[0:2]
+				hum = int(h_data,16)/10.0
+				logging.debug('Lywsd03------ Advertising Data=> Moist: ' + str(hum))
+				action['moisture'] = hum
+			elif val_type in ['0a']: # type: battery
+				b_data = val_data[0:2]
+				batt = int(b_data,16)
+				logging.debug('Lywsd03------ Advertising Data=> Batt: ' + str(batt))
+				action['battery'] = batt
+			elif val_type in ['0d']: # type: temp&moist
+				 t_data = val_data[2:4] + val_data[0:2]
+				 temp = int(t_data,16)/10.0
+				 h_data = val_data[6:8] + val_data[4:6]
+				 hum = int(h_data,16)/10.0
+				 logging.debug('Lywsd03------ Advertising Data=> Temp: ' + str(temp) + ' Moist: ' + str(hum))
+				 action['temperature'] = temp
+				 action['moisture'] = hum
 		return action
 
 	def read(self,mac):
