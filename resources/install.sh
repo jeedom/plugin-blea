7ed2835a68caa9a316b8f7da4c6b6feba00e69dd
Erwan Nozay <erwan.nozay@orange.com>
2020-10-24 10:48:04 +0200

    Update install with pip3

--- resources/install.sh
+++ resources/install.sh
@@ -3,17 +3,18 @@ echo 0 > /tmp/dependancy_blea_in_progress
 echo "Launch install of blea dependancy"
 sudo apt-get update
 echo 50 > /tmp/dependancy_blea_in_progress
-sudo apt-get install -y python-pip python-dev build-essential python-requests bluetooth libffi-dev libssl-dev
+sudo apt-get install -y python3-pip python-dev build-essential python-requests bluetooth libffi-dev libssl-dev
 echo 66 > /tmp/dependancy_blea_in_progress
 sudo apt-get install -y libglib2.0-dev git
 echo 75 > /tmp/dependancy_blea_in_progress
-sudo pip install pyudev
-sudo pip install pyserial
-sudo pip install requests
+sudo pip3 install pyudev
+sudo pip3 install pyserial
+sudo pip3 install requests
+sudo pip3 install bluepy
 echo 80 > /tmp/dependancy_blea_in_progress
-sudo pip install cryptography
+sudo pip3 install cryptography
 echo 90 > /tmp/dependancy_blea_in_progress
-sudo pip install pycrypto
+sudo pip3 install pycrypto
 echo 95 > /tmp/dependancy_blea_in_progress
 cd /tmp
 sudo rm -R /tmp/bluepy >/dev/null 2>&1
