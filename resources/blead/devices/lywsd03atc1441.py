4f8674c4fcafffb238a211a49608acbb389c116e
Tancrède SUARD <gitlab.commits@tancou.me>
2023-08-14 20:09:28 +0200

    Make a new model in the UI

--- resources/blead/devices/lywsd03atc1441.py
+++ resources/blead/devices/lywsd03atc1441.py
@@ -7,21 +7,39 @@ import struct
 from multiconnect import Connector
 from notification import Notification
 
-class Lywsd03():
+class Lywsd03Atc1441():
 	def __init__(self):
-		self.name = 'lywsd03'
+		self.name = 'lywsd03atc1441'
 		self.ignoreRepeat = False
 
 	def isvalid(self,name,manuf='',data='',mac=''):
 		if name.lower() in [self.name]:
 			return True
-		if data.lower().startswith("95fe") and (mac.lower().startswith("a4:c1:38")):
-			#broadcasted advertising data
+		if data.lower().startswith("1a18") and (mac.lower().startswith("a4:c1:38")):
+			#broadcasted advertising data from custom firmware
+			# https://github.com/atc1441/ATC_MiThermometer#advertising-format-of-the-custom-firmware
 			return True
 
 	def parse(self,data,mac,name,manuf):
 		action={}
 		action['present'] = 1
+		# broadcasted advertising data from custom firmware
+		# https://github.com/atc1441/ATC_MiThermometer#advertising-format-of-the-custom-firmware
+		# Also compatible with the recommended custom firmware made by pvvx
+		# At https://github.com/pvvx/ATC_MiThermometer > atc1441 mode
+		logging.debug("Lywsd03Atc1441 PARSE data: %s for %s " % (data, len(data)) )
+		if (data.lower().startswith("1a18") and len(data) == 30):
+			received = data[16:]
+			logging.debug('Lywsd03Atc1441 PARSE received: %s' % received )
+			temp = int.from_bytes(bytearray.fromhex(received[0:4]), byteorder='big', signed=True) / 10.0
+			logging.debug('Lywsd03Atc1441------ Advertising Data=> Temp: ' + str(temp))
+			action['temperature'] = temp
+			hum = int(received[4:6],16)
+			logging.debug('Lywsd03Atc1441------ Advertising Data=> Moist: ' + str(hum))
+			action['moisture'] = hum
+			batt = int(received[6:8],16)
+			logging.debug('Lywsd03Atc1441------ Advertising Data=> Batt: ' + str(batt))
+			action['battery'] = batt
 		return action
 
 	def read(self,mac):
@@ -41,7 +59,7 @@ class Lywsd03():
 			notification.subscribe(10)
 			result['battery'] = battery
 			result['id'] = mac
-			logging.debug('LYWSD03------'+str(result))
+			logging.debug('Lywsd03Atc1441------'+str(result))
 			return result
 		except Exception as e:
 			logging.error(str(e))
@@ -59,4 +77,4 @@ class Lywsd03():
 			result['source'] = globals.daemonname
 			globals.JEEDOM_COM.add_changes('devices::'+conn.mac,result)
 
-globals.COMPATIBILITY.append(Lywsd03)
+globals.COMPATIBILITY.append(Lywsd03Atc1441)
